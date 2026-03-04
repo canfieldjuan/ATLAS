dac52d864a5973588dcb357c8f468a94915bcaa1
Juan Canfield <canfieldjuan24@gmail.com>
2026-03-04 15:42:52 -0600

    blog: auto-publish microsoft-teams-deep-dive-2026-03

--- atlas-churn-ui/src/content/blog/index.ts
+++ atlas-churn-ui/src/content/blog/index.ts
@@ -81,6 +81,7 @@ import mondaycomDeepDive202603 from './mondaycom-deep-dive-2026-03'
 import linodeDeepDive202603 from './linode-deep-dive-2026-03'
 import metabaseDeepDive202603 from './metabase-deep-dive-2026-03'
 import crowdstrikeDeepDive202603 from './crowdstrike-deep-dive-2026-03'
+import microsoftTeamsDeepDive202603 from './microsoft-teams-deep-dive-2026-03'
 export interface ChartSpec {
   chart_id: string
   chart_type: 'bar' | 'horizontal_bar' | 'radar' | 'line'
@@ -186,4 +187,5 @@ export const POSTS: BlogPost[] = [
   linodeDeepDive202603,
   metabaseDeepDive202603,
   crowdstrikeDeepDive202603,
+  microsoftTeamsDeepDive202603,
 ].sort((a, b) => b.date.localeCompare(a.date))
