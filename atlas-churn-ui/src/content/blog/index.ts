33206e2c9b3219b54f53a48bb62dc4fcb9d7f5a9
Juan Canfield <canfieldjuan24@gmail.com>
2026-03-04 15:44:32 -0600

    blog: auto-publish sentinelone-deep-dive-2026-03

--- atlas-churn-ui/src/content/blog/index.ts
+++ atlas-churn-ui/src/content/blog/index.ts
@@ -85,6 +85,7 @@ import microsoftTeamsDeepDive202603 from './microsoft-teams-deep-dive-2026-03'
 import workdayDeepDive202603 from './workday-deep-dive-2026-03'
 import ripplingDeepDive202603 from './rippling-deep-dive-2026-03'
 import fortinetDeepDive202603 from './fortinet-deep-dive-2026-03'
+import sentineloneDeepDive202603 from './sentinelone-deep-dive-2026-03'
 export interface ChartSpec {
   chart_id: string
   chart_type: 'bar' | 'horizontal_bar' | 'radar' | 'line'
@@ -194,4 +195,5 @@ export const POSTS: BlogPost[] = [
   workdayDeepDive202603,
   ripplingDeepDive202603,
   fortinetDeepDive202603,
+  sentineloneDeepDive202603,
 ].sort((a, b) => b.date.localeCompare(a.date))
