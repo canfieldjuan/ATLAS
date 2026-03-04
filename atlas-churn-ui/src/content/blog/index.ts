eada7863cbfec93984d6844f02fde5640683c7b5
Juan Canfield <canfieldjuan24@gmail.com>
2026-03-04 17:35:18 -0600

    blog: auto-publish notion-vs-trello-2026-03

--- atlas-churn-ui/src/content/blog/index.ts
+++ atlas-churn-ui/src/content/blog/index.ts
@@ -115,6 +115,7 @@ import basecampVsNotion202603 from './basecamp-vs-notion-2026-03'
 import asanaDeepDive202603 from './asana-deep-dive-2026-03'
 import mondaycomVsNotion202603 from './mondaycom-vs-notion-2026-03'
 import notionVsWrike202603 from './notion-vs-wrike-2026-03'
+import notionVsTrello202603 from './notion-vs-trello-2026-03'
 export interface ChartSpec {
   chart_id: string
   chart_type: 'bar' | 'horizontal_bar' | 'radar' | 'line'
@@ -254,4 +255,5 @@ export const POSTS: BlogPost[] = [
   asanaDeepDive202603,
   mondaycomVsNotion202603,
   notionVsWrike202603,
+  notionVsTrello202603,
 ].sort((a, b) => b.date.localeCompare(a.date))
