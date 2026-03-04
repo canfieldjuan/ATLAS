66aecb2ac5c3890eafe309f7403fb5c79734e015
Juan Canfield <canfieldjuan24@gmail.com>
2026-03-04 15:16:59 -0600

    blog: auto-publish klaviyo-deep-dive-2026-03

--- atlas-churn-ui/src/content/blog/index.ts
+++ atlas-churn-ui/src/content/blog/index.ts
@@ -65,6 +65,7 @@ import jiraDeepDive202603 from './jira-deep-dive-2026-03'
 import trelloDeepDive202603 from './trello-deep-dive-2026-03'
 import smartsheetDeepDive202603 from './smartsheet-deep-dive-2026-03'
 import pipedriveDeepDive202603 from './pipedrive-deep-dive-2026-03'
+import klaviyoDeepDive202603 from './klaviyo-deep-dive-2026-03'
 export interface ChartSpec {
   chart_id: string
   chart_type: 'bar' | 'horizontal_bar' | 'radar' | 'line'
@@ -154,4 +155,5 @@ export const POSTS: BlogPost[] = [
   trelloDeepDive202603,
   smartsheetDeepDive202603,
   pipedriveDeepDive202603,
+  klaviyoDeepDive202603,
 ].sort((a, b) => b.date.localeCompare(a.date))
