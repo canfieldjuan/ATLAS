36b3e84ecf7fc99a4a8bc04aab68dd363068dbbd
Juan Canfield <canfieldjuan24@gmail.com>
2026-03-04 14:41:04 -0600

    blog: auto-publish intercom-deep-dive-2026-03

--- atlas-churn-ui/src/content/blog/index.ts
+++ atlas-churn-ui/src/content/blog/index.ts
@@ -40,6 +40,7 @@ import googleCloudDeepDive202603 from './google-cloud-deep-dive-2026-03'
 import whyTeamsLeaveSlack202603 from './why-teams-leave-slack-2026-03'
 import bestCrmFor150202603 from './best-crm-for-1-50-2026-03'
 import topComplaintEveryCrm202603 from './top-complaint-every-crm-2026-03'
+import intercomDeepDive202603 from './intercom-deep-dive-2026-03'
 export interface ChartSpec {
   chart_id: string
   chart_type: 'bar' | 'horizontal_bar' | 'radar' | 'line'
@@ -104,4 +105,5 @@ export const POSTS: BlogPost[] = [
   whyTeamsLeaveSlack202603,
   bestCrmFor150202603,
   topComplaintEveryCrm202603,
+  intercomDeepDive202603,
 ].sort((a, b) => b.date.localeCompare(a.date))
