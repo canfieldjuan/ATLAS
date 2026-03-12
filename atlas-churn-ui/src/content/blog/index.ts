d9485f8bde141a6ee9dcaf4a24b44ff22fa92ea9
Juan Canfield <canfieldjuan24@gmail.com>
2026-03-12 15:01:56 -0500

    blog: auto-publish insightly-vs-salesforce-2026-03

--- atlas-churn-ui/src/content/blog/index.ts
+++ atlas-churn-ui/src/content/blog/index.ts
@@ -9,6 +9,7 @@ import bamboohrVsRippling202603 from './bamboohr-vs-rippling-2026-03'
 import realCostOfSalesforce202603 from './real-cost-of-salesforce-2026-03'
 import jiraVsTeamwork202603 from './jira-vs-teamwork-2026-03'
 import freshsalesVsSalesforce202603 from './freshsales-vs-salesforce-2026-03'
+import insightlyVsSalesforce202603 from './insightly-vs-salesforce-2026-03'
 type ChartValue = string | number | null | undefined
 type ChartDatum = Record<string, ChartValue>
 
@@ -79,4 +80,5 @@ export const POSTS: BlogPost[] = [
   realCostOfSalesforce202603,
   jiraVsTeamwork202603,
   freshsalesVsSalesforce202603,
+  insightlyVsSalesforce202603,
 ]
